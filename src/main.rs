d48808613ef259e26b58c66c68ffa5c1fa4a8ade
xiejw <xiejw.io+dt@gmail.com>
2024-10-25 13:42:43 -0700

    [main] Added logger.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use env_logger::Env;
+use log::info;
+
 fn main() {
-    println!("Hello, world!");
+    // Init env_logger.
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    info!("Hello, world!");
 }
